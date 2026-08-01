81a84f2fd5c8c2d45ace555f457a0b6607df6575
Dannylo Torres <krdannylo.new@gmail.com>
2026-08-01 20:25:04 -0300

    ⚙️ build: add entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+echo "Preparando Ambiente.."
+
+until pg_isready -h db -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+  echo "Aguardando PostgreSQL.."
+  sleep 4
+done
+echo "Banco de dados Pronto!"
+
+echo "Executando Prisma Generate"
+npx prisma generate
+
+echo "Executando Migrations.."
+if ! npx prisma migrate deploy; then
+  echo "Algo falhou na execução das migrations."
+  exit 1
+fi
+
+echo "Iniciando Aplicação!"
+npm run start:dev
